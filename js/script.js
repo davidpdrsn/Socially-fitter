c19314f8fd487a65cb1260d529ce7e890fc34fef
DKvistgaard <daniel@kvistgaards.dk>
2012-05-24 14:16:03 +0200

    Added a lot of styles on the profile

--- js/script.js
+++ js/script.js
@@ -10,8 +10,8 @@ function hideAddressBar() {
 jQuery.fn.fadeSliderToggle = function(settings) {
  	/* Damn you jQuery opacity:'toggle' that dosen't work!~!!!*/
  	 settings = jQuery.extend({
-		speed:300,
-		easing : "swing"
+		speed: 300,
+		easing: "swing"
 	}, settings)
 	
 	caller = this
