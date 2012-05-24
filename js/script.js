ff690094d09d29a19d28d0cfe044c0e23c6a8f30
DKvistgaard <daniel@kvistgaards.dk>
2012-05-24 11:45:55 +0200

    More styling on the comment box

--- js/script.js
+++ js/script.js
@@ -6,6 +6,30 @@ function hideAddressBar() {
   window.scrollTo(0, 1);
 }
 
+
+jQuery.fn.fadeSliderToggle = function(settings) {
+ 	/* Damn you jQuery opacity:'toggle' that dosen't work!~!!!*/
+ 	 settings = jQuery.extend({
+		speed:300,
+		easing : "swing"
+	}, settings)
+	
+	caller = this
+ 	if($(caller).css("display") == "none"){
+ 		$(caller).animate({
+ 			opacity: 1,
+ 			height: 'toggle'
+ 		}, settings.speed, settings.easing);
+	}else{
+		$(caller).animate({
+ 			opacity: 0,
+ 			height: 'toggle'
+ 		}, settings.speed, settings.easing);
+	}
+};
+
+
+
 $(document).ready(function() {
 
   $('.log').click(function() {
@@ -14,6 +38,22 @@ $(document).ready(function() {
 
   $('span.addmore').click(function(){
     alert("Aww! You hit me!");
+  });
+  
+  $('.commenting-log').click(function(){
+    $(this).parent().parent().parent('.log');
+    return false;
+  });
+  $('.log-comment a').click(function(){
+    $(this).parent().parent().parent('.log');
+    return false;
+  });
+  
+  $('.log-comment a').click(function(){
+    var $commentinglog = $(this).parent().parent().children('.commenting-log');
+    $(this).parent().toggleClass('clicked');   
+		$commentinglog.fadeSliderToggle();
+		return false;
   });
 
 });
