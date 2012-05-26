609f4593232ad4c46fb88ee9bed0e1bb8c7d9732
DKvistgaard <daniel@kvistgaards.dk>
2012-05-27 00:29:19 +0200

    Added some js

--- js/script.js
+++ js/script.js
@@ -6,7 +6,27 @@ function hideAddressBar() {
   window.scrollTo(0, 1);
 }
 
-
+// fading and slidetoggle
+jQuery.fn.fadeSliderToggle = function(settings) {
+ 	/* Damn you jQuery opacity:'toggle' that dosen't work!~!!!*/
+ 	 settings = jQuery.extend({
+		speed: 300,
+		easing: "swing"
+	}, settings)
+	
+	caller = this
+ 	if($(caller).css("display") == "none"){
+ 		$(caller).animate({
+ 			opacity: 1,
+ 			height: 'toggle'
+ 		}, settings.speed, settings.easing);
+	}else{
+		$(caller).animate({
+ 			opacity: 0,
+ 			height: 'toggle'
+ 		}, settings.speed, settings.easing);
+	}
+};
 
 $(document).ready(function() {
   
