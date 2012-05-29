38f46c4218efde986f0ca4babc31452f7f995a91
David Pedersen <david.pdrsn@gmail.com>
2012-05-29 14:12:48 +0200

    added autoload function to controller.php

--- inc/controller.php
+++ inc/controller.php
@@ -2,4 +2,9 @@
 // display all errors
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
+
+// automatically load classes when they are requested
+function __autoload($class_name) {
+  include_once 'inc/class.' . $class_name . '.inc.php';
+}
 ?>
