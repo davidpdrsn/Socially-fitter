14494a3bf4976fcc7aab222cc9975835d5823dba
David Pedersen <david.pdrsn@gmail.com>
2012-05-30 15:37:31 +0200

    Differences between production and development

--- phptest.php
+++ phptest.php
@@ -1,21 +1,5 @@
 <?php
-require_once("inc/controller.php");
+
 
 ?>
 
-<?php if($session->is_logged_in()): ?>
-  <p>
-    You are logged in!
-  </p>
-  <p>
-    <a href="logout.php">Logout</a>
-  </p>
-<?php else: ?>
-  <form action="login.php" method="post">
-    <label for="username">Username: </label><input type="text" name="username">
-    <br>
-    <label for="password">Password: </label><input type="password" name="password">
-    <br>
-    <input type="submit" name="submit" value="login">
-  </form>
-<?php endif; ?>
