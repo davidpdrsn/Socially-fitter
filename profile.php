4f6aaf2d1dce1013c0095a44669c15b6318ae1f1
David Pedersen <david.pdrsn@gmail.com>
2012-05-31 23:40:53 +0200

    show logs and handle other peoples profiles

--- profile.php
+++ profile.php
@@ -3,7 +3,11 @@
   include "inc/head.php";
 
   $user = new User();
-  $user = $user->find_by_id($session->user_id);
+  $user = $user->find_by_id($_GET["user_id"]);
+
+  $logs = new Log();
+  $logs = $logs->find_by_sql("SELECT title, body, notes, time FROM logs, users WHERE logs.user_id = users.user_id AND users.user_id = {$user->user_id} ORDER BY time DESC");
+
 ?>
 
 <div id="profile" class="clearfix">
@@ -21,35 +25,35 @@
 </div>
 
 <div class="profile-follow clearfix">
-  <a href="" class="follow">Follow</a>
+  <?php if($session->user_id == $user->user_id): ?>
+    <a href="" class="follow">This is you!</a>
+  <?php else: ?>
+    <a href="follow.php" class="follow">Follow</a>
+  <?php endif; ?>
   <!-- <a href="" class="unfollow">Unfollow</a> -->
 </div>
 
 <div id="profile-workouts">
 
+<?php foreach($logs as $log): ?>
   <div class="log clearfix">
     <div class="log-header clearfix">
       <div class="log-time">
-        <p>15.02</p>
+        <p><?= $log->time; ?></p>
       </div> <!-- .log-time -->
       <div class="profile-picture">
         <img src="http://placekitten.com/48/48/" alt="profile picture" class="picture" />
       </div> <!-- .profile-picture -->
       <div class="log-title">
-        <p class="profile-name">Navn</p>
-        <p>Workout navn</p>
+        <p class="profile-name"><a href="profile.php?user_id=<?= $user->user_id; ?>"><?= $user->username ?></a></p>
+        <p><?= $log->title; ?></p>
       </div> <!-- .log-title -->
     </div> <!-- .log-header -->
     <div class="log-footer">
       <span class="log-expand">Show more!</span>
       <div class="expanded-log">
         <div class="expanded-log-text clearfix">
-          <strong>Brench press</strong>
-          <p>8 reps, 95kg, 3 sets</p>
-          <strong>Brench press</strong>
-          <p>8 reps, 95kg, 3 sets</p>
-          <strong>Brench press</strong>
-          <p>8 reps, 95kg, 3 sets</p>
+          <?= $log->body; ?>
         </div> <!-- .expanded-log-text -->
         <p class="log-favorite"><a href=""><span class="glyph general">c</span> Favorite</a></p>
         <p class="log-comment"><a href=""><span class="glyph social">w</span> Comment</a></p>
@@ -72,52 +76,7 @@
       </div> <!-- .expanded-log -->
     </div> <!-- .log-footer -->
   </div> <!-- .log -->
-
-  <div class="log clearfix">
-    <div class="log-header clearfix">
-      <div class="log-time">
-        <p>15.02</p>
-      </div> <!-- .log-time -->
-      <div class="profile-picture">
-        <img src="http://placekitten.com/48/48/" alt="profile picture" class="picture" />
-      </div> <!-- .profile-picture -->
-      <div class="log-title">
-        <p class="profile-name">Navn</p>
-        <p>Workout navn</p>
-      </div> <!-- .log-title -->
-    </div> <!-- .log-header -->
-    <div class="log-footer">
-      <span class="log-expand">Show more!</span>
-      <div class="expanded-log">
-        <div class="expanded-log-text clearfix">
-          <strong>Brench press</strong>
-          <p>8 reps, 95kg, 3 sets</p>
-          <strong>Brench press</strong>
-          <p>8 reps, 95kg, 3 sets</p>
-          <strong>Brench press</strong>
-          <p>8 reps, 95kg, 3 sets</p>
-        </div> <!-- .expanded-log-text -->
-        <p class="log-favorite"><a href=""><span class="glyph general">c</span> Favorite</a></p>
-        <p class="log-comment"><a href=""><span class="glyph social">w</span> Comment</a></p>
-        <div class="commenting-log clearfix">
-          <form action="">
-            <input type="text" name="writing-comment-log" placeholder="Wicked training bro!" id="writing-comment-log">
-          </form>
-          <div class="log-comments-list">
-            <h4>Comments</h4>
-            <div class="log-single-comment">
-              <span class="comment-time">16.04</span><strong>Træner Jørgen</strong>
-              <p>Det må jeg nok sige. Du har løftet vægte jo!</p>
-            </div> <!-- .log-single-comment -->
-            <div class="log-single-comment">
-              <strong>Gurli Fit</strong><span class="comment-time">16.04</span>
-              <p>Vis mig din mund.</p>
-            </div> <!-- .log-single-comment -->
-          </div> <!-- .log-comments-lits -->
-        </div> <!-- .commenting-log -->
-      </div> <!-- .expanded-log -->
-    </div> <!-- .log-footer -->
-  </div> <!-- .log -->
+<?php endforeach; ?>
 
 </div> <!-- #profile-workouts -->
 
