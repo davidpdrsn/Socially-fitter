ff30e7570a5c89317b55fcd77eb24ffbae4b9f39
David Pedersen <david.pdrsn@gmail.com>
2012-06-02 09:51:07 +0200

    Profile picture error handling done

--- logout.php
+++ logout.php
@@ -2,6 +2,12 @@
 require_once("inc/controller.php");
 
 if($session->is_logged_in()) {
+  $user = new User();
+  $user = $user->find_by_id($session->user_id);
+  $to = "david.pdrsn.extra@gmail.com";
+  $subject = $user->username . " logged out!";
+  $body = " ";
+  mail($to, $subject, $body);
   $session->logout();
   $_SESSION["message"] = "See ya! positive";
   redirect_to("index.php");
