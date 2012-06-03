1916b87f0a22a02d7b75de2bda1083ad3e278ee0
DKvistgaard <daniel@kvistgaards.dk>
2012-06-03 21:10:16 +0200

    smooth scroll on index

--- js/script.js
+++ js/script.js
@@ -152,55 +152,13 @@ $(document).ready(function() {
 
 
   //Smooth scrolling
-  function filterPath(string) {
-    return string
-      .replace(/^\//,'')
-      .replace(/(index|default).[a-zA-Z]{3,4}$/,'')
-      .replace(/\/$/,'');
-  }
-  var locationPath = filterPath(location.pathname);
-  
-
-  $('a[href*=#]').each(function() {
-    $(this).click(function() {
-      var scrollElem = scrollableElement('html', 'body');
-      var thisPath = filterPath(this.pathname) || locationPath;
-      if (  locationPath == thisPath
-      && (location.hostname == this.hostname || !this.hostname)
-      && this.hash.replace(/#/,'') ) {
-        var $target = $(this.hash), target = this.hash;
-        if (target) {
-          var firstTargetOffset = $target.offset().top;
-          var targetOffset = firstTargetOffset;
-          $(this).click(function(event) {
-            event.preventDefault();
-            $(scrollElem).animate({scrollTop: targetOffset}, 400, function() {
-              location.hash = target;
-            });
-          });
-        }
-      }
-    });
+  $('#logotype a').click(function(e) {
+    e.preventDefault();
+    var targetOffset = $('#seewhy').offset().top;
+    $('html, body').animate({
+      scrollTop: targetOffset
+    }, 400);
   });
-  // use the first element that is "scrollable"
-  function scrollableElement(els) {
-    for (var i = 0, argLength = arguments.length; i <argLength; i++) {
-      var el = arguments[i],
-          $scrollElement = $(el);
-      if ($scrollElement.scrollTop()> 0) {
-        return el;
-      } else {
-        $scrollElement.scrollTop(1);
-        var isScrollable = $scrollElement.scrollTop()> 0;
-        $scrollElement.scrollTop(0);
-        if (isScrollable) {
-          return el;
-        }
-      }
-    }
-    return [];
-  }
-  //End of smooth scrolling
 
   $('#share .facebook, #share .twitter').click(function() {
     $('#share .done-sharing').text("No thanks" ? "I'm done sharing" : "No thanks");
